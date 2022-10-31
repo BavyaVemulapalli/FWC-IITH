208861b497376b06683e970e19388f2b070622df
Bavya Vemulapalli <111565327+BavyaVemulapalli@users.noreply.github.com>
2022-10-31 22:24:48 +0530

    Update line.py

--- Line/Code/line.py
+++ Line/Code/line.py
@@ -2,28 +2,28 @@ import numpy as np
 import math
 from matplotlib import pyplot as plt, patches
 from matplotlib.patches import Polygon
-t1 = np.array([[-1,0], [0,0], [3,5.19]])
-x1=t1[2,0]
-y1=t1[2,1]
-x2=t1[1,0]
-y2=t1[1,1]
-slope=(y2-y1)/(x2-x1)
-print("Slope of QR = Tan(x) = ",'%.2f'%slope)
-theta=round(math.degrees(math.atan(slope)))
+t1 = np.array([[-1,0], [0,0], [3,3*math.sqrt(3)]])
+M = ((t1[1])-(t1[2]))
+print('------------------------')
+print("Direction vector of QR = ",M)
+M=M/M[0]
+m=M[1]
+print('------------------------')
+print("Slope of direction vector QR = ",'%.2f'%m)
+theta=round(math.degrees(math.atan(m)))
+print('------------------------')
 print("Angle of QR with origin = ",theta)
 pqr=int(180-theta)
+print('------------------------')
 print("Angle PQR = ",pqr)
 r = math.radians(pqr)
 q = math.tan(r)
 qm = round(q,2)
+print('------------------------')
 print("Slope of the angular bisector of PQR = ", qm)
-print("Equation of the angular bisecor is  Y = ",qm,"X")
+print('------------------------')
+print("Equation of the angular bisecor is  Y = ",qm,"*X")
 p1 = Polygon(t1, closed=True)
-point1 = [0,0]
-point2 = [-1.5,2.59]
-x_values = [point1[0], point2[0]]
-y_values = [point1[1], point2[1]]
-plt. plot(x_values, y_values)
 ax = plt.gca()
 ax.add_patch(p1)
 ax.set_xlim(1,10)
@@ -38,7 +38,9 @@ plt.annotate("P", (-1,0))
 plt.annotate("Q", (0,0))
 plt.annotate("R", (3,5.19))
 plt.annotate("M", (-0.7,1.21))
+plt.plot([0,-1.5],[0,2.59], color='blue')
 plt.scatter(-0.7,1.21, color='brown')
 plt.plot([-0.7,-0.7],[0,1.21],color='orange')
 plt.plot([-0.7,0.69],[1.21,1.21],color='orange')
-plt.show()
+plt.savefig('/sdcard/Github/Line/Images/diagram.png')
+subprocess.run(shlex.split("termux-open /sdcard/Github/Line/Images/diagram.png"))
